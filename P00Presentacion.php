042a72bc3b9bcd6fc14b49ac2fd5115cf9663480
Gael <gaelgarciamena703@gmail.com>
2024-10-21 09:29:07 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,16 @@
+<?php
+/*CBTIs 89
+P00Presentacion.php
+Programa que contiene datos personales
+Garcia Mena Gael Esau
+Programacion 3roA TM 
+*/
+
+echo "Garcia Mena Gael Esau <br>";
+echo "16 Años <br>";
+echo "Estudia en el CBTIs 89 <br>";
+echo "En la carrera de Programacion <br>";
+echo "Del 3er semestre del grupo A turno matutino <br>";
+echo "De Durango Dgo <br>";
+echo "Telefono: 618 158 2937 <br>";
+?>
